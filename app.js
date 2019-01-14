5ea6cce1f59b6dce06affe190c61be9d5a5a7578
Leooliveri <leonardo.oliveri@studenti.unitn.it>
2019-01-14 16:43:07 +0100

    added app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 
 const app =express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true})
 
+app.get('/square',(req,res) => {
+    var his_string = req.query.string;
+    console.log(his_string);
+});
 
 module.exports = app;
